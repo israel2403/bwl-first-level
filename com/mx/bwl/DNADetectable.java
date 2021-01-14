57097504bc3a0213871afde40f70a5b52e61ccda
israel2403 <israelhf24@gmail.com>
2021-01-14 10:21:36 -0800

    02 DELETE unnecesary imports from DNADetectable interface

--- com/mx/bwl/DNADetectable.java
+++ com/mx/bwl/DNADetectable.java
@@ -1,11 +1,6 @@
 package com.mx.bwl;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-import java.util.stream.Collectors;
 
 /**
  * @author : isra
